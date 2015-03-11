762cce785531b130a1a226b0f9bee07b4729e0e4
Richard Schneider <makaretu@gmail.com>
2015-03-11 19:56:44 +1300

    #1 operation can be a computer column.  Also [where] can default to connection's IP address.

--- audit.sql
+++ audit.sql
@@ -1,12 +1,17 @@
 create table audit
 (
     audit_id int identity(1,1) not null primary key, 
-    [operation] char not null, 
+	[operation] as case
+	  when old_content is null and new_content is not null then 'insert'
+	  when old_content is not null and new_content is not null then 'update'
+	  when old_content is not null and new_content is null then 'delete'
+	  when old_content is null and new_content is null then 'select'
+	end,
     table_name varchar(128) not null, 
     old_content xml null, 
     new_content xml null, 
     who nvarchar(128) not null default system_user, 
     [when] datetimeoffset not null default sysdatetimeoffset(), 
-    [where] nvarchar(128) null, 
+    [where] nvarchar(128) null default convert(nvarchar, connectionproperty('client_net_address')), 
     why nvarchar(128) null
 )
--- trigger-template.sql
+++ trigger-template.sql
@@ -1,31 +1,20 @@
 --
 -- This a template for a yact auditing trigger.
 -- You MUST replace the following strings
---  - {table} with the name of the table to audit
---  - {table_id} with the name of the table's primary key
+--  - {table_name} with the name of the table to audit
+--  - {table_key} with the name of the table's primary key
 --
-create trigger [trg_{table}]
-on {table} for insert, update, delete not for replication
+create trigger [trg_{table_name}]
+on {table_name} for insert, update, delete not for replication
 as
 begin
 	set NOCOUNT on
 
-	-- TODO: operation should be a computed value based on the nullness of old_content and new_content
-	declare @operation as char(1)
-	if exists (select * from inserted)
-	if exists (select * from deleted)
-		select @operation = 'U'
-	else
-		select @operation = 'I'
-	else
-		select @operation = 'D'
-
-  insert into audit (operation, table_name, old_content, new_content) 
-  select
-    	@operation,
-    	'{table}',
-	case when d.{table_id} is null then null else (select d.* for xml raw) end,
-	case when i.{table_id} is null then null else (select i.* for xml raw) end
-	from inserted as i
-	full outer join deleted as d on i.{table_id} = d.{table_id}
+    insert into audit (table_name, old_content, new_content)
+    select
+    	'{table_name}',
+	    case when d.{table_key} is null then null else (select d.* for xml raw) end,
+	    case when i.{table_key} is null then null else (select i.* for xml raw) end
+	    from inserted as i
+	    full outer join deleted as d on i.{table_key} = d.{table_key}
 end
